54e97ac7996aae50d7621fe2d10d1ca465a75345
lucasdufour <lucas.dufour@insa-lyon.fr>
2023-12-11 21:32:11 +0100

    Switch from python to js

---
+++ scripts/process_issue.js
@@ -0,0 +1,48 @@
+const { Octokit } = require("@octokit/core");
+const { OpenAIAPI } = require("openai");
+
+async function main() {
+    const githubToken = process.env.GITHUB_TOKEN;
+    const openaiKey = process.env.OPENAI_API_KEY;
+    const aiModel = process.env.AI_MODEL || 'gpt-4-1106-preview';
+    const maxTokens = parseInt(process.env.MAX_TOKENS || '1000');
+    const githubRepo = process.env.GITHUB_REPOSITORY;
+    const issueNumber = parseInt(process.env.GITHUB_ISSUE_NUMBER);
+
+    const octokit = new Octokit({ auth: githubToken });
+    const openai = new OpenAIAPI({ apiKey: openaiKey });
+
+    try {
+        // Get issue information
+        const { data: issue } = await octokit.request(`GET /repos/${githubRepo}/issues/${issueNumber}`);
+        const userLogin = issue.user.login;
+        const originalIssueText = issue.body;
+
+        // Construct the AI prompt
+        const aiPrompt = `Please rephrase the following issue to make it clearer and provide additional context where necessary:\n${originalIssueText}\n\n` +
+                         `Identify the type of this issue. Is it a documentation update, a bug report, a feature request, or something else?\n\n` +
+                         `Tag the user @${userLogin} who created this issue and list a few questions that could help clarify the issue for better understanding and resolution.`;
+
+        // OpenAI ChatCompletion request
+        const response = await openai.createChatCompletion({
+            model: aiModel,
+            messages: [
+                { role: "system", content: "You are a helpful assistant experienced in software development and issue tracking. Your task is to analyze GitHub issues, provide clear and concise summaries, categorize them accurately (e.g., bug, feature request, documentation), and generate relevant questions to clarify each issue for better understanding." },
+                { role: "user", content: aiPrompt }
+            ],
+            max_tokens: maxTokens
+        });
+
+        // Update the issue with the response
+        await octokit.request(`POST /repos/${githubRepo}/issues/${issueNumber}/comments`, {
+            body: `AI-Assisted Response:\n${response.choices[0].message.content}`
+        });
+    } catch (error) {
+        console.error("Error processing issue:", error);
+    }
+}
+
+main().catch(error => {
+    console.error("Error in main function:", error);
+    process.exit(1);
+});
